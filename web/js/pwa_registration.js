6778a5cf34f88fcbb056356ac7315a0da74baa42
SebastiansIT <sebastian@human-injection.de>
2020-12-06 18:33:59 +0100

    Link serviceworker relative to root

--- web/js/pwa_registration.js
+++ web/js/pwa_registration.js
@@ -1,7 +1,7 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
 	// The service worker file has to be in the web root for security reasons
-	navigator.serviceWorker.register('serviceworker.js')
+	navigator.serviceWorker.register('/serviceworker.js')
 	.then((registration) => {
 		console.log(`Registration successful, scope is: ${registration.scope}`);
 	})
