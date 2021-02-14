616eeb99aac5106382dbe9e8d41bc06bb65f0d13
SebastiansIT <sebastian@human-injection.de>
2021-02-14 11:24:24 +0100

    Set path to service worker script relative to wallaset folder

--- app/Resources/static/themes/_global/js/pwaregistration.js
+++ app/Resources/static/themes/_global/js/pwaregistration.js
@@ -1,7 +1,7 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
   // The service worker file has to be in the web root for security reasons
-  navigator.serviceWorker.register('/serviceworker.js')
+  navigator.serviceWorker.register('../serviceworker.js')
     .catch((error) => {
       console.error(`Service worker registration failed, error: ${error}`);
     });
